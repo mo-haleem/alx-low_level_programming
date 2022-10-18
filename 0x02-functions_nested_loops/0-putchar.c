db05e0c4703cdfba95b80a804680007d4ded279f
Eniola <ennyolasupo@gmail.com>
2022-10-18 14:16:39 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ *
+ *  * main - Print "_putschar" followed by a new line.
+ *
+ *   * Description: You are not allowed to include standard libraries.
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+	char _putchar[] = "putchar";
+
+	int i = 0;
+
+	while (_putchar[i] != '\0')
